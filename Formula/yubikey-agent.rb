2e2e60ec04cee75cc93774ec8f66c26f7d810f9b
Sebastian Kurfürst <sebastian.kurfuerst@sandstorm.de>
2020-07-23 08:36:08 +0200

    WIP: use custom built yubikey-agent version

--- Formula/yubikey-agent.rb
+++ Formula/yubikey-agent.rb
@@ -1,24 +1,18 @@
 # Forked yubikey-agent from https://github.com/FiloSottile/yubikey-agent/blob/47bc9321572e2a15a18ed8060a4537f15339ef4c/HomebrewFormula/yubikey-agent.rb
-# YOU NEED TO INSTALL: https://gpgtools.org/
 # WHAT HAS CHANGED?
-# - removed pinentry-mac dependency
-# - changed PATH to add /usr/local/MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/
+# - using https://github.com/skurfuerst/yubikey-agent/releases/tag/custom-password-caching binary
 
 class YubikeyAgent < Formula
     desc "Seamless ssh-agent for YubiKeys"
     homepage "https://filippo.io/yubikey-agent"
-    url "https://github.com/FiloSottile/yubikey-agent/archive/v0.1.3.zip"
-    sha256 "1c6c85f3b280b1acaef5f8a70d1e01bc173a5a7bd913eef8beddfbd5b2dccb26"
+    url "https://github.com/skurfuerst/yubikey-agent/releases/download/custom-password-caching/yubikey-agent-osx.zip"
+    sha256 "8c88f9bf0ff8fabe8fe2d4a61159aba46f425c297bfaeb205882038d8760afb0"
     head "https://filippo.io/yubikey-agent", :using => :git
   
-    depends_on "go" => :build
+    depends_on "pinentry-mac"
   
     def install
-      ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
-      mkdir bin
-      system "go", "build", "-trimpath", "-ldflags", "-X main.Version=v#{version}",
-          "-o", bin, "filippo.io/yubikey-agent"
-      prefix.install_metafiles
+      bin.install "sku"
     end
   
     def post_install
@@ -37,8 +31,7 @@ class YubikeyAgent < Formula
           <key>EnvironmentVariables</key>
           <dict>
             <key>PATH</key>
-            <string>/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/</string>
-          </dict>
+            <string>/usr/bin:/bin:/usr/sbin:/sbin:#{Formula["pinentry-mac"].opt_bin}</string>          </dict>
           <key>ProgramArguments</key>
           <array>
             <string>#{opt_bin}/yubikey-agent</string>
